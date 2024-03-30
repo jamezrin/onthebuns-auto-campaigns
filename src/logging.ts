1b2aa99a1b28eca17c4f5b093ce161464174a6df
Jaime Martínez Rincón <jaime@jamezrin.name>
2024-03-30 01:39:53 +0100

    chore: tweak logging

--- src/logging.ts
+++ src/logging.ts
@@ -1,38 +1,8 @@
 import winston from 'winston';
 
-export function setupLogger() {
-  const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-      winston.format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss',
-      }),
-      winston.format.errors({ stack: true }),
-      winston.format.splat(),
-      winston.format.json(),
-    ),
-    defaultMeta: { app: 'onthatass-auto-campaigns' },
-    transports: [new winston.transports.Console()],
-  });
-
-  if (process.env.NODE_ENV !== 'production') {
-    logger.add(
-      new winston.transports.Console({
-        format: winston.format.combine(
-          winston.format.colorize(),
-          winston.format.timestamp(),
-          customDevWinstonFormat,
-        ),
-      }),
-    );
-  }
-
-  return logger;
-}
-
-export const customDevWinstonFormat = winston.format.printf(
-  ({ app, level, message, timestamp, ...rest }) => {
-    const base = `[${app}] ${timestamp} ${level}: ${message}`;
+export const hybridJsonFormat = winston.format.printf(
+  ({ app: _, level, message, ...rest }) => {
+    const base = `[${level}]: ${message}`;
 
     if (Object.keys(rest).length === 0) {
       return base;
@@ -41,3 +11,30 @@ export const customDevWinstonFormat = winston.format.printf(
     return base + '\r\n' + JSON.stringify(rest, null, 2);
   },
 );
+
+export const prdFormat = winston.format.combine(
+  winston.format.timestamp({
+    format: 'YYYY-MM-DD HH:mm:ss',
+  }),
+  winston.format.errors({ stack: true }),
+  winston.format.splat(),
+  winston.format.json(),
+);
+
+export const devFormat = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.errors({ stack: true }),
+  winston.format.splat(),
+  hybridJsonFormat,
+);
+
+export function setupLogger() {
+  const logger = winston.createLogger({
+    level: 'info',
+    format: process.env.NODE_ENV === 'production' ? prdFormat : devFormat,
+    defaultMeta: { app: 'onthatass-auto-campaigns' },
+    transports: [new winston.transports.Console()],
+  });
+
+  return logger;
+}
